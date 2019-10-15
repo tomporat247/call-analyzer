766522c07eecf159ec6158b1531a8912ea4a8cf1
DESKTOP-73GVL8A\Tom <tomporat247@gmail.com>
2019-10-15 15:11:31 +0300

    save all call log data

--- lib/call_log/services/call_log_parser_service.dart
+++ lib/call_log/services/call_log_parser_service.dart
@@ -1,17 +1,21 @@
 import 'package:call_log/call_log.dart';
 
 class CallLogParserService {
-  final String _name = 'n';
+  final String _name = 'nm';
   final String _callType = 'c';
   final String _duration = 'd';
   final String _timeStamp = 't';
+  final String _number = 'n';
+  final String _formattedNumber = 'fn';
 
   dynamic callLogEntryToMap(dynamic callLogEntry) {
     return {
       _name: callLogEntry.name,
       _callType: callLogEntry.callType.index,
       _duration: callLogEntry.duration,
-      _timeStamp: callLogEntry.timestamp
+      _timeStamp: callLogEntry.timestamp,
+      _number: callLogEntry.number,
+      _formattedNumber: callLogEntry.formattedNumber
     };
   }
 
@@ -20,6 +24,8 @@ class CallLogParserService {
         name: value[_name],
         callType: CallType.values[value[_callType]],
         duration: value[_duration],
-        timestamp: value[_timeStamp]);
+        timestamp: value[_timeStamp],
+        number: value[_number],
+        formattedNumber: value[_formattedNumber]);
   }
 }
