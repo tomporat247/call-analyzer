f13598ef19947c8052a3dad8774c93744b93203d
DESKTOP-73GVL8A\Tom <tomporat247@gmail.com>
2019-10-15 21:43:52 +0300

    finish async sort, store maps to ease future computations

--- lib/analysis/services/analysis_service.dart
+++ lib/analysis/services/analysis_service.dart
@@ -6,51 +6,66 @@ import 'package:contacts_service/contacts_service.dart';
 class AnalysisService {
   List<Contact> _contacts;
   List<CallLogEntry> _callLogs;
+  Map<Contact, List<CallLogEntry>> _contactToCallLogs;
+  Map<Contact, int> _contactToCallDurationInSeconds;
+  Map<Contact, int> _contactToCallAmount;
 
   init(List<Contact> contacts, List<CallLogEntry> callLogs) {
-    _contacts = contacts;
+    _contactToCallDurationInSeconds = new Map<Contact, int>();
+    _contactToCallLogs = new Map<Contact, List<CallLogEntry>>();
+    _contactToCallAmount = new Map<Contact, int>();
     _callLogs = callLogs;
+    _contacts = contacts;
+
+    _contacts.forEach((Contact contact) =>
+        _contactToCallLogs[contact] = _getAllCallLogsForContact(contact));
+
+    _contacts.forEach((Contact contact) =>
+        _contactToCallDurationInSeconds[contact] =
+            _getTotalCallDurationWith(contact).inSeconds);
+
+    _contacts.forEach((Contact contact) =>
+        _contactToCallAmount[contact] = _getTotalCallAmountWith(contact));
   }
 
-  _getSortedContacts(SortOption sortOption) {
+  Future<List<Contact>> _getSortedContacts(SortOption sortOption) {
     List<Contact> contactsCopy = List.from(_contacts);
     switch (sortOption) {
       case SortOption.CALL_DURATION:
-        _sortContactsByCallDuration(contactsCopy);
-        break;
+        return _sortContactsByCallDuration(contactsCopy);
       case SortOption.CALL_AMOUNT:
-        _sortContactsByCallAmount(contactsCopy);
-        break;
+        return _sortContactsByCallAmount(contactsCopy);
       case SortOption.ALPHABETICAL:
-        _sortContactsByName(contactsCopy);
-        break;
+        return _sortContactsByName(contactsCopy);
+      default:
+        return null;
     }
-    return contactsCopy;
   }
 
-  _sortContactsByCallDuration(List<Contact> contacts) {
-    contacts.sort((Contact one, Contact two) => _getTotalCallDurationWith(two)
-        .compareTo(_getTotalCallDurationWith(one)));
+  Future<List<Contact>> _sortContactsByCallDuration(
+      List<Contact> contacts) async {
+    return asyncSort(contacts, (Contact c) => _contactToCallDurationInSeconds[c]);
   }
 
-  _sortContactsByCallAmount(List<Contact> contacts) {
-    contacts.sort((Contact one, Contact two) =>
-        _getTotalCallAmountWith(two).compareTo(_getTotalCallAmountWith(one)));
+  Future<List<Contact>> _sortContactsByCallAmount(
+      List<Contact> contacts) async {
+    return asyncSort(contacts, (Contact c) => _contactToCallAmount[c]);
   }
 
-  _sortContactsByName(List<Contact> contacts) {
-    contacts.sort((Contact one, Contact two) =>
-        two.displayName.compareTo(one.displayName));
+  Future<List<Contact>> _sortContactsByName(List<Contact> contacts) async {
+    return contacts
+      ..sort((Contact one, Contact two) =>
+          one.displayName.compareTo(two.displayName));
   }
 
   Duration _getTotalCallDurationWith(Contact contact) {
     return Duration(
-        seconds: _getAllCallLogsForContact(contact).fold<int>(
+        seconds: _contactToCallLogs[contact].fold<int>(
             0, (int curr, CallLogEntry callLog) => curr + callLog.duration));
   }
 
   int _getTotalCallAmountWith(Contact contact) {
-    return _getAllCallLogsForContact(contact).length;
+    return _contactToCallLogs[contact].length;
   }
 
   List<CallLogEntry> _getAllCallLogsForContact(Contact contact,
--- lib/helper/helper.dart
+++ lib/helper/helper.dart
@@ -1,3 +1,34 @@
+import 'package:flutter/foundation.dart';
+
+typedef extractCompareByFromElement<Element, Comparer> = Comparer Function(
+    Element element);
+
 formatPhoneNumber(String phoneNumber) {
   return phoneNumber == null ? null : phoneNumber.replaceAll('-', '');
+}
+
+Future<List<Element>> asyncSort<Element, Comparer>(
+    List<Element> originalList,
+    extractCompareByFromElement<Element, Comparer> compareBy) async {
+  return (await compute(
+          _getSortedIndexes, originalList.map<Comparer>(compareBy).toList()))
+      .map((dynamic index) => originalList[index])
+      .toList();
+}
+
+List<int> _getSortedIndexes<T>(List<T> list) {
+  String valueKey = 'val';
+  String indexKey = 'idx';
+  int indexCounter = 0;
+
+  List<dynamic> sortedListWithIndexes = list
+      .map((T item) => {valueKey: item, indexKey: indexCounter++})
+      .toList()
+        ..sort((dynamic one, dynamic two) =>
+            two[valueKey].compareTo(one[valueKey]));
+
+  return sortedListWithIndexes
+      .map((dynamic item) => item[indexKey])
+      .cast<int>()
+      .toList();
 }
