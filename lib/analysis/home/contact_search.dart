47fcb64909b6e4e6ed70a8cde255e600dc8421ca
DESKTOP-73GVL8A\Tom <tomporat247@gmail.com>
2019-10-19 17:31:59 +0300

    fix search bar color problem

--- lib/analysis/home/contact_search.dart
+++ lib/analysis/home/contact_search.dart
@@ -1,15 +1,14 @@
 import 'package:call_analyzer/analysis/services/analysis_service.dart';
-import 'package:call_analyzer/config.dart';
 import 'package:call_analyzer/widgets/contact_image.dart';
 import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
-// TODO: Search string color is white and can't be seen
-
 class ContactSearch extends SearchDelegate<Contact> {
   AnalysisService _analysisService;
   List<Contact> _topContacts;
+  final TextStyle contactNameTextStyle =
+      TextStyle(color: Colors.white70.withOpacity(0.9));
 
   ContactSearch() {
     _analysisService = GetIt.instance<AnalysisService>();
@@ -17,6 +16,21 @@ class ContactSearch extends SearchDelegate<Contact> {
     _analysisService
         .getTopContacts(amount: 10)
         .then((List<Contact> topContacts) => _topContacts = topContacts);
+  }
+
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return theme.copyWith(
+        inputDecorationTheme: InputDecorationTheme(
+            hintStyle: TextStyle(color: theme.primaryTextTheme.title.color)),
+        primaryColor: theme.primaryColor,
+        primaryIconTheme: theme.primaryIconTheme,
+        primaryColorBrightness: theme.primaryColorBrightness,
+        primaryTextTheme: theme.primaryTextTheme,
+        textTheme: theme.textTheme.copyWith(
+            title: theme.textTheme.title
+                .copyWith(color: theme.primaryTextTheme.title.color)));
   }
 
   @override
@@ -66,12 +80,17 @@ class ContactSearch extends SearchDelegate<Contact> {
           leading: ContactImage(contact),
           title: RichText(
             text: TextSpan(children: [
-              TextSpan(text: contact.displayName.substring(0, boldStartIndex)),
+              TextSpan(
+                  text: contact.displayName.substring(0, boldStartIndex),
+                  style: contactNameTextStyle),
               TextSpan(
                   text: contact.displayName
                       .substring(boldStartIndex, boldEndIndex),
-                  style: TextStyle(fontWeight: FontWeight.bold, color: getAppTheme(context).accentColor)),
-              TextSpan(text: contact.displayName.substring(boldEndIndex)),
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.white)),
+              TextSpan(
+                  text: contact.displayName.substring(boldEndIndex),
+                  style: contactNameTextStyle),
             ]),
           ),
         );
